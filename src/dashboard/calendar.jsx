0d0a7f9cd9cff263010eef05a8d2fe793c52b6d9
William Cao <wcao00@stuy.edu>
2018-12-30 17:14:47 -0500

    hover changes view

--- src/dashboard/calendar.jsx
+++ src/dashboard/calendar.jsx
@@ -4,15 +4,47 @@ class Calendar extends Component{
     constructor(props){
         super(props);
         this.state = {
+            active: false,
+
+            // Default view is the one that is active without the mouse hovering over it
             title: "CALENDAR",
             date: new Date().getDate(),
             dayOfWeek: Calendar.getDayOfWeek()
+
+            // Extra view is the one that is active when the mouse is hovering over it
+
         };
     }
 
     render() {
         return(
-            <div style={this.getWrapperStyle()}>
+            <div style={this.getWrapperStyle()} onMouseEnter={this.showActive} onMouseLeave={this.showInactive}>
+                {this.state.active ? this.getActiveView() : this.getInactiveView()}
+            </div>
+        );
+    }
+
+    /**
+     * Updates the state so the inactive view will be shown. The inactive view contains minimal information.
+     */
+    showInactive = () => {
+        this.setState({
+            active: false
+        });
+    };
+
+    /**
+     * Updates the state so the active view will be shown. The active view contains more information for the user.
+     */
+    showActive = () => {
+        this.setState({
+            active: true
+        });
+    };
+
+    getInactiveView(){
+        return(
+            <React.Fragment>
                 <h1 style={this.getHeaderStyle(35)}>
                     {this.state.title}
                 </h1>
@@ -22,7 +54,13 @@ class Calendar extends Component{
                 <h1 style={this.getHeaderStyle(30)}>
                     {this.state.dayOfWeek}
                 </h1>
-            </div>
+            </React.Fragment>
+        );
+    }
+
+    getActiveView(){
+        return(
+            <React.Fragment/>
         );
     }
 
