f47c4b29d41771a62fd1e255b21b0e8cefc7568d
William Cao <wcao00@stuy.edu>
2018-12-29 22:32:09 -0500

    renamed using .jsx and use calendar component

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Calendar from './calendar.jsx';
 import './dashboard.css';
 
 class Dashboard extends Component{
     render() {
         return(
-
+            <Calendar />
         )
     }
 }
