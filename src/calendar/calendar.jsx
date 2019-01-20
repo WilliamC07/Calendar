1958aa36cfbc864e9c3e64f17aad7085429dad38
William Cao <wcao00@stuy.edu>
2019-01-19 23:16:04 -0500

    skeleton for calendar

---
+++ src/calendar/calendar.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+const util = require('./util.js');
+
+class Calendar extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Calendar;
