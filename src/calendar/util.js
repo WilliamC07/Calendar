46c81add3ae6f442e64cd1cd5cb70bda2f61c790
William Cao <wcao00@stuy.edu>
2019-04-06 17:24:35 -0400

    check for undefined

--- src/calendar/util.js
+++ src/calendar/util.js
@@ -15,6 +15,8 @@ function getSunday(date){
 
 function equalDates(date1, date2){
     return (
+        date1 !== undefined &&
+        date2 !== undefined &&
         date1.getFullYear() === date2.getFullYear() &&
         date1.getMonth() === date2.getMonth() &&
         date1.getDate() === date2.getDate()
