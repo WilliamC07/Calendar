7848c9670bb226412543efa11473ba24fc22057e
William Cao <wcao00@stuy.edu>
2019-04-10 15:19:20 -0400

    refractor: cleaned up code

--- src/data.js
+++ src/data.js
@@ -1,87 +1,39 @@
-/*
- * This file should be used to gain data that was stored on disk.
- */
 const path = require('path');
-const fs = window.require('fs');
+const fs = require('fs');
 
-function getProgramDirectory(){
-    const programDirectoryPath = path.join("/Users/williamcao/Library", "Application Support", "CalendarJS");
-    createDirectoryIfMissing(programDirectoryPath);
-    return programDirectoryPath;
-}
+/**
+ * Head directory of the program.
+ * @type {string} path to the program directory
+ */
+const programDirectory = path.join(require('os').homedir(), "Library", "Application Support", "CalendarJS");
+createDirectoryIfMissing(programDirectory);
 
-function getCalendarDirectory(programDirectory){
-    const calendarPath = path.join(programDirectory, 'Calendar');
-    createDirectoryIfMissing(calendarPath);
-    return calendarPath;
+/**
+ * All the subdirectories from the head directory of the program.
+ * To add another directory, just follow the format in the object. The directory will be automatically created if it
+ * does not exist.
+ * @type {{calendarDirectory: string}}
+ */
+const subDirectories = {
+    calendarDirectory: path.join(programDirectory, "calendar"),
+};
+
+/* create directories if missing */
+for(let subDirectory in subDirectories){
+    createDirectoryIfMissing(subDirectories[subDirectory]);
 }
 
 /**
- * Makes a directory for the given path if one does not exist. If one does exist, nothing will happen.
- * @param directoryPath Path to the directory that may or may not exist
+ * Creates the directory given if one does not exists.
+ * @param directoryPath String representation of the path to the directory
  */
 function createDirectoryIfMissing(directoryPath){
-    // Sync because we need a place to store data
     if(!fs.existsSync(directoryPath)){
         fs.mkdirSync(directoryPath);
     }
 }
 
-function readFile(filePath, parser) {
-    if (!fs.existsSync(filePath)) {
-        // create the file if one doesn't exists
-        fs.writeFileSync(filePath, "{}");
-        return null;
-    } else {
-        console.log(fs.readFileSync(filePath, 'utf8'));
-        return JSON.parse(fs.readFileSync(filePath, 'utf8'), parser);
-    }
-}
-
-function writeFile(filePath, jsonString){
-    fs.writeFileSync(filePath, jsonString);
-}
-
-function readConfig(){
-    const information = {};
-
-
-    return information;
-}
-
-class Data{
-    constructor(){
-        const programDirectory = getProgramDirectory();
-        this._calendarPath = getCalendarDirectory(programDirectory);
-    }
-
-    getCalendarData(fileName){
-        // credit: https://weblog.west-wind.com/posts/2014/Jan/06/JavaScript-JSON-Date-Parsing-and-real-Dates
-        const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
-        const reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
-
-        // the parser will convert the json representation of a date object to a date object
-        return readFile(path.join(this._calendarPath, fileName), (key, value) => {
-            if (typeof value === 'string') {
-                let a = reISO.exec(value);
-                if (a)
-                    return new Date(value);
-                a = reMsAjax.exec(value);
-                if (a) {
-                    const b = a[1].split(/[-+,.]/);
-                    return new Date(b[0] ? +b[0] : 0 - +b[1]);
-                }
-            }
-            return value;
-        });
-    }
-
-    setCalendarData(fileName, jsonString){
-        writeFile(path.join(this._calendarPath, fileName), jsonString);
-    }
-}
-
-// Create a single instance
-const data = new Data();
-
-export default data;
+module.exports = {
+    calendarDirectory: () => subDirectories.calendarDirectory,
+    createFileIfMissing: (filePath) => fs.writeFile(filePath, "", (err) => {if(err) throw err}),
+};
