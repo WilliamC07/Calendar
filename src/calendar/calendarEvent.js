90e850b1d19129d6482676981e2b0aebbec66832
William Cao <wcao00@stuy.edu>
2019-04-12 18:03:02 -0400

    can add events

--- src/calendar/calendarEvent.js
+++ src/calendar/calendarEvent.js
@@ -9,12 +9,34 @@ const events = [];
  * This number is available for the next eventID.
  * @type {number}
  */
-let avaibleEventID = 0;
+let availableID = 0;
 /**
- * Stores all the date and their eventID pairs.
+ * Stores all the date and their eventID pairs. The date key should only have the year and month and month value.
+ * Everything else (hour, minute, second, millisecond) should be 0.
  * @type {{Date: [int]}}
  */
 const dateEventIDs = {};
 
+/* Functions to use */
+/**
+ * Add the event.
+ * @param event Event to add.
+ */
+function addEvent(event){
+    event.eventID = availableID++;
+
+    // Add the dateEventIDs pair
+    let startDate = new Date(event._dateStart);
+    while(equalDates(startDate, event._dateEnd)){
+        if(dateEventIDs[startDate] === undefined){
+            dateEventIDs[startDate] = [event.eventID];
+        }else{
+            dateEventIDs[startDate].push(event.eventID);
+        }
+        startDate.setDate(startDate.getDate() + 1);
+    }
+
+    events.push(event);
+}
 
 export {}
