6e2d0b43ad25c13dfc7f296144b0d969f6baf80c
William Cao <wcao00@stuy.edu>
2019-07-22 21:01:23 -0400

    added jsx for spent and earned fields

--- src/money/dateView.jsx
+++ src/money/dateView.jsx
@@ -10,12 +10,30 @@ function DateView({startingDate}){
         return parts;
     }
 
+    function earnedFields(){
+        const parts = [];
+        for(let day = 0; day < 7; day++){
+            parts.push(<h3 onClick={() => console.log("clicked!")}>$0</h3>)
+        }
+        return parts;
+    }
+
+    function spentFields(){
+        const parts = [];
+        for(let day = 0; day < 7; day++){
+            parts.push(<h3 onClick={() => console.log("clicked!")}>$0</h3>)
+        }
+        return parts;
+    }
+
     return (
         <div id="dateViewContainer">
             <div/>
             {daysOfWeekLabel()}
             <h3>Earned</h3>
+            {earnedFields()}
             <h3>Spent</h3>
+            {spentFields()}
         </div>
     )
 }
