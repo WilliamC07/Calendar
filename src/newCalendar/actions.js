af80753b5eb1beaf86c88c3b92c341fccba7cd7e
William Cao <wcao00@stuy.edu>
2019-08-10 18:14:42 -0400

    added category actions

--- src/newCalendar/actions.js
+++ src/newCalendar/actions.js
@@ -7,10 +7,33 @@ export function setMonthYearSelected(moment){
         moment: moment
     }
 }
-
 export function setDaySelected(moment){
     return {
         type: SET_DAY_SELECTED,
         moment: moment
+    }
+}
+
+
+export const ADD_CATEGORY = "ADD_CATEGORY";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const DELETE_CATEGORY = "DELETE_CATEGORY";
+
+export function addCategory(category){
+    return {
+        type: ADD_CATEGORY,
+        category: category
+    }
+}
+export function updateCategory(category){
+    return {
+        type: UPDATE_CATEGORY,
+        category: category
+    }
+}
+export function deleteCategory(category){
+    return {
+        type: DELETE_CATEGORY,
+        category: category
     }
 }
--- src/store/calendarReducer.js
+++ src/store/calendarReducer.js
@@ -1,12 +1,17 @@
 import {
     SET_DAY_SELECTED,
     SET_MONTH_YEAR_SELECTED,
+    ADD_CATEGORY,
+    UPDATE_CATEGORY,
+    DELETE_CATEGORY,
 } from "../newCalendar/actions";
 import moment from "moment";
+import category from "../money/category";
 
 const initialState = {
     monthYearSelected: moment(),
     daySelected: moment(),
+    categories: []
 };
 
 export default function calendarReducer(state = initialState, action){
@@ -20,6 +25,21 @@ export default function calendarReducer(state = initialState, action){
             return {
                 ...state,
                 monthYearSelected: action.moment,
+            };
+        case ADD_CATEGORY:
+            return {
+                ...state,
+                categories: [...state.categories, action.category]
+            };
+        case UPDATE_CATEGORY:
+            return {
+                ...state,
+                categories: [...state.categories.filter(c => c.id !== action.category.id), action.category]
+            };
+        case DELETE_CATEGORY:
+            return {
+                ...state,
+                categories: state.categories.filter(c => c.id !== action.category.id)
             };
         default:
             return state;
