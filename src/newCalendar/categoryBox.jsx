d710ec258d501f254ef8da8dbcd1f18c9463ba01
William Cao <wcao00@stuy.edu>
2019-08-09 23:09:09 -0400

    added fields for creating and modifying categories

--- src/newCalendar/categoryBox.jsx
+++ src/newCalendar/categoryBox.jsx
@@ -1,10 +1,32 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import Category from "../money/category";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlus} from "@fortawesome/free-solid-svg-icons/faPlus";
 
 function CategoryBoxConnect({}){
     // todo: initial state is the last one opened
     const [categorySelected, setCategorySelected] = useState("School");
+    const [isCreatingNewField, setIsCreatingNewField] = useState(false);
+    const [categoryName, setCategoryName] = useState("");
+    const [categoryColor, setCategoryColor] = useState("");
+    const [categoryDesc, setCategoryDesc] = useState("");
+
+    function cancelCreation(){
+        if(isCreatingNewField){
+            // todo: go back to last selected item
+        }else{
+            // todo: go back to using existing
+        }
+    }
+
+    function createCategory(){
+
+    }
+
+    function updateCategory(){
+
+    }
 
     return (
         <div className="categoryBox">
@@ -14,7 +36,25 @@ function CategoryBoxConnect({}){
                     <option value="Entertainment">Entertainment</option>
                     <option value="Work">Work</option>
                 </select>
+                <FontAwesomeIcon icon={faPlus} fixedWidth/>
             </div>
+            <form>
+                <div className="inputGroup">
+                    <label>Name</label>
+                    <input type="text"/>
+                </div>
+                <div className="inputGroup">
+                    <label>Color</label>
+                </div>
+                <div className="inputGroup">
+                    <label>Description</label>
+                    <input type="text" value={categoryDesc}/>
+                </div>
+                <div>
+                    <input type="button" value={isCreatingNewField ? "create" : "update"}/>
+                    <input type="button" value="cancel" onClick={cancelCreation}/>
+                </div>
+            </form>
         </div>
     )
 }
