d8551f5d5fb9559e69b7a30f52c03aecd65e7631
William Cao <wcao00@stuy.edu>
2019-08-16 23:21:22 -0400

    new logic for creating, deleting, showing, and updating categories

--- src/newCalendar/categoryBox.jsx
+++ src/newCalendar/categoryBox.jsx
@@ -1,14 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
-import Category from "../money/category";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPlus} from "@fortawesome/free-solid-svg-icons/faPlus";
 import {calendarData} from '../data/data';
 
 function CategoryBoxConnect({categories, updateCategory, deleteCategory, addCategory, getCategories}){
-    // todo: initial state is the last one opened
-    const [categorySelected, setCategorySelected] = useState(1);
-    const [isCreatingNewField, setIsCreatingNewField] = useState(false);
+    // none: no category selected | create: new event made | view: old event viewing
+    const [mode, setMode] = useState("none");
+    // -1 for "none" or "create" mode | other positive integers for view
+    const [selectedCategoryID, setSelectedCategoryID] = useState(1);
     const [categoryName, setCategoryName] = useState("");
     const [categoryColor, setCategoryColor] = useState("");
     const [categoryDesc, setCategoryDesc] = useState("");
@@ -21,46 +21,84 @@ function CategoryBoxConnect({categories, updateCategory, deleteCategory, addCate
             firstUpdate.current = false;
         }
     });
-
-    function cancelCreation(e){
-        e.preventDefault();
-        if(isCreatingNewField){
-            // todo: go back to last selected item
-        }else{
-            // todo: go back to using existing
+    // update view if categories update
+    useEffect(() => {
+        if(categories.length > 0){
+            const lastCategory = categories[categories.length - 1];
+            setSelectedCategoryID(lastCategory.id);
+            setCategoryAttributeToState(lastCategory);
+            setMode("view");
         }
+    }, [categories]);
+
+    function chooseCategoryHandler(e){
+        const targetID = parseInt(e.target.value);
+        setSelectedCategoryID(targetID);
+        setCategoryAttributeToState(categories.filter(c => c.id.toString() === targetID.toString())[0]);
     }
-
-    function createOrUpdate(e){
-        e.preventDefault();
-        if(isCreatingNewField){
-            addCategory({name: categoryName, color: categoryColor, description: categoryDesc});
-            // go back to viewing previous element
-            setIsCreatingNewField(false);
-        }else{
-
-        }
-    }
-
-    function setCreatingEventStatus(){
-        setIsCreatingNewField(!isCreatingNewField)
+    function createNewCategoryHandler(e){
+        setMode("create");
         setCategoryName("");
         setCategoryColor("");
         setCategoryDesc("");
+    }
+    function createOrUpdateHandler(e){
+        e.preventDefault();
+        const createdCategory = {name: categoryName, color: categoryColor, description: categoryDesc};
+        if(mode === "create"){
+            // id will be set automatically from the database when added
+            addCategory(createdCategory);
+        }else{
+            // don't modify category directly (don't modify state directly), create copy instead
+            createdCategory.id = selectedCategoryID;
+            updateCategory(createdCategory);
+        }
+    }
+    function cancelCreationHandler(e){
+        e.preventDefault();
+        if(mode === "create"){
+            setMode("view")
+        }else{
+            setCategoryAttributeToState(categories.filter(c => c.id === selectedCategoryID)[0]);
+        }
+    }
+    function deleteCategoryHandler(e){
+        e.preventDefault();
+        const toDelete = selectedCategoryID;
+        if(categories.length <= 1){
+            // no other event to choose after we delete
+            setMode("none");
+        }else{
+            // select the first category we find that isn't the one we are going to delete
+            for(const category of categories){
+                if(category.id.toString() !== selectedCategoryID.toString()){
+                    setSelectedCategoryID(category.id);
+                    break;
+                }
+            }
+        }
+        deleteCategory(toDelete);
+    }
+
+    function setCategoryAttributeToState(category){
+        setCategoryName(category.name);
+        setCategoryColor(category.color);
+        setCategoryDesc(category.description);
     }
 
     return (
         <div className="categoryBox">
             <form>
                 <div className="choosingCategory">
-                    <select value={isCreatingNewField ? "-1" : categorySelected} onChange={(e) => setCategorySelected(e.target.value)}>
-                        {categories.map(category => <option value={category.id}
+                    <select value={mode === "view" ? selectedCategoryID.toString() : "-1"} onChange={chooseCategoryHandler}>
+                        {mode === "view" && categories.map(category => <option value={category.id}
                                                             key={category.name + category.id}>{category.name}</option>)}
-                        {isCreatingNewField && <option value="-1">New Category</option>}
+                        {mode === "create" && <option value="-1">New Category</option>}
+                        {mode === "none" && <option value="-1">Nothing Selected</option>}
                     </select>
-                    <label htmlFor="newEventButton" className={isCreatingNewField ? "selectedText" : "regularText"}
-                           onClick={setCreatingEventStatus}>
-                        New Category<FontAwesomeIcon icon={faPlus} fixedWidth id="newEventButton" size="sm"/>
+                    <label className={mode==="create" ? "selectedText" : "regularText"}
+                           onClick={createNewCategoryHandler}>
+                        New Category<FontAwesomeIcon icon={faPlus} fixedWidth size="sm"/>
                     </label>
                 </div>
                 <div className="inputGroup">
@@ -74,10 +112,14 @@ function CategoryBoxConnect({categories, updateCategory, deleteCategory, addCate
                     <label>Description:</label>
                     <input type="text" value={categoryDesc} onChange={(e) => setCategoryDesc(e.target.value)}/>
                 </div>
-                <div className="inputGroup">
-                    <button className="create" onClick={createOrUpdate}>{isCreatingNewField ? "Create" : "Update"}</button>
-                    <button className="cancel" onClick={cancelCreation}>Cancel</button>
-                </div>
+                {
+                    mode !== "none" &&
+                        <div className="inputGroup">
+                        <button className="create" onClick={createOrUpdateHandler}>{mode === "create" ? "Create" : "Update"}</button>
+                        <button className="cancel" onClick={cancelCreationHandler}>Cancel</button>
+                        {mode === "view" && <button className="cancel" onClick={deleteCategoryHandler}>Delete</button>}
+                    </div>
+                }
             </form>
         </div>
     )
@@ -87,6 +129,7 @@ function CategoryBoxConnect({categories, updateCategory, deleteCategory, addCate
 function mapStateToProps(store){
     return {
         categories: store.calendar.categories,
+        selectedCategoryID: store.calendar.selectedCategoryID,
     }
 }
 
