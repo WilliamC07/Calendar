5532e6c77440b6208037c4263e477cda40d02c75
William Cao <wcao00@stuy.edu>
2019-09-02 18:47:31 -0400

    header for new event maker

--- src/newCalendar/eventViewer/newEventViewerBox.jsx
+++ src/newCalendar/eventViewer/newEventViewerBox.jsx
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
+import {faChevronUp} from "@fortawesome/free-solid-svg-icons/faChevronUp";
+import "./design.scss";
+import {connect} from "react-redux";
 
-export default function NewEventViewerBox({}){
+function NewEventViewerBoxConnect({}) {
     const [expanded, setExpanded] = useState(false);
     const [eventInfo, setEventInfo] = useState({
         name: "",
@@ -11,8 +16,34 @@ export default function NewEventViewerBox({}){
     };
 
     return (
-        <div>
+        <div className="newEventContainer">
+            <div className="headerContainer">
+                <h2>New Event</h2>
+                <button onClick={() => setExpanded(!expanded)}>
+                    <FontAwesomeIcon icon={expanded ? faChevronUp : faChevronDown} fixedWidth size="lg"/>
+                </button>
+            </div>
+            {expanded &&
+                <div className="makerContainer">
 
+                </div>
+            }
         </div>
     )
 }
+
+function mapStateToProps(store){
+    return {
+
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+
+    }
+}
+
+const NewEventViewerBox = connect(mapStateToProps, mapDispatchToProps)(NewEventViewerBoxConnect);
+
+export default NewEventViewerBox;
