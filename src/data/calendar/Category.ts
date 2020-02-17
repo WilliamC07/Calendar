360be411c9036a9738bd74f7dfc1e8f1fe1b37c8
William Cao <wcao00@stuy.edu>
2020-02-17 15:58:05 -0500

    use typescript for calendar classes

--- src/data/calendar/Category.ts
+++ src/data/calendar/Category.ts
@@ -1,11 +1,21 @@
+/**
+ * Every {@link Event} must have a Category.
+ *
+ * Relationship: Many to one
+ */
 export default class Category{
+    id: number;
+    name: string;
+    color: string;
+    description: string;
+
     /**
-     * Creates a Category
-     * @param name {string} Name of the category
-     * @param color {string} Hexadecimal representation with leading '#'. Example: "#ffffff"
-     * @param description {string} Description of the category
+     * Creates a new category
+     * @param name Name of the category
+     * @param color Color of the category. Formatted as "#xxxxxx"
+     * @param description Description of the category
      */
-    constructor(name, color, description){
+    constructor(name: string, color: string, description: string){
         this.id = -1; // this will be set when we insert into the sqlite database
         this.name = name;
         this.color = color;
--- src/data/calendar/Event.ts
+++ src/data/calendar/Event.ts
@@ -1,8 +1,15 @@
-import moment from 'moment';
+import moment, {Moment} from "moment";
 
 export default class Event{
+    id: number;
+    title: string;
+    description: string;
+    category: number;
+    isAllDay: boolean;
+    start: string;
+    end: string;
+
     /**
-     *
      * @param title {string}
      * @param description {string}
      * @param category {number} Matches an id of a category
@@ -10,7 +17,7 @@ export default class Event{
      * @param start {string} ISOString of the starting time
      * @param end {string} ISOString of the ending time
      */
-    constructor(title, description, category, isAllDay, start, end){
+    constructor(title: string, description: string, category: number, isAllDay: boolean, start: string, end: string){
         this.id = -1; // this will be set when we insert into the sqlite database
         this.title = title;
         this.description = description;
@@ -20,11 +27,11 @@ export default class Event{
         this.end = end;
     }
 
-    get startTime(){
+    get startTime(): Moment{
         return moment(this.start);
     }
 
-    get endTime(){
+    get endTime(): Moment{
         return moment(this.end);
     }
 
