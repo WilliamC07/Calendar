b7341ba829a90cc418e294ad7800bfd4d5fb1a63
William Cao <wcao00@stuy.edu>
2020-02-18 17:15:46 -0500

    stricter type checking

--- src/calendar/event.ts
+++ src/calendar/event.ts
@@ -13,17 +13,19 @@ export default class Event{
      * @param title {string}
      * @param description {string}
      * @param category {number} Matches an id of a category
-     * @param isAllDay {number} If the event is all day. 1 for true. 0 for false
+     * @param isAllDay {boolean} If the event is all day.
      * @param start {moment.Moment} Starting time
      * @param end {moment.Moment} Ending time
      */
-    constructor(title: string, description: string, category: number, isAllDay: boolean | number, start: string | Moment, end: string | Moment){
+    constructor(title: string, description: string, category: number, isAllDay: boolean, start: Moment, end: Moment){
         this.id = -1; // this will be set when we insert into the sqlite database
         this.title = title;
         this.description = description;
         this.category = category;
-        this.isAllDay = !!isAllDay;
-        this.start = moment(start);
-        this.end = moment(end);
+        this.isAllDay = isAllDay;
+        Object.freeze(start);
+        Object.freeze(end);
+        this.start = start;
+        this.end = end;
     }
 }
