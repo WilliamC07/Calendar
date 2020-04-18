56191847658ca3de7d905fd34efe0938c2438e6f
William Cao <wcao00@stuy.edu>
2020-04-17 21:55:11 -0400

    renamed to corner-calendar

--- src/calendar/views/index.tsx
+++ src/calendar/views/index.tsx
@@ -3,7 +3,7 @@ import CategoryBox from "./category/category-box";
 import NewEventViewerBox from "./event-creator/newEventViewerBox";
 import EventContainer from "./event-viewer/event-container";
 import "./style.scss";
-import {CornerCalendar} from "./calendar";
+import {CornerCalendar} from "./corner-calendar/corner-calendar";
 
 export default function Index(){
     return (
