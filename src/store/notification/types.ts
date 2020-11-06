0edca6d201b3beb49aac9d3f22f8d88e52e4f07f
willcao <willcao@umich.edu>
2020-11-06 17:38:46 -0500

    removed unused constants and types declaration

--- src/store/notification/types.ts
+++ src/store/notification/types.ts
@@ -1,12 +1,4 @@
-import {Notification} from "../../notification/notification";
-
-export const NOTIFICATION_SLICE_NAME = "notifications";
-
 export enum NotificationTypes {
     ADD_NOTIFICATION = "@@notifications/ADD_NOTIFICATION",
     REMOVE_NOTIFICATION = "@@notifications/REMOVE_NOTIFICATION"
-}
-
-export interface NotificationState {
-    readonly notifications: Notification[]
 }
